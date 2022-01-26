9fb0cb19f263fae6a6b6a150b6df4f777e88a256
Aalim aslam <67648366+aalimaslam@users.noreply.github.com>
2022-01-26 17:41:57 +0530

    adding OG Tags

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:title" content="Best Recipe App" />
+    <meta property="og:description" content="This is the best web app to find recipe to your favorite meal" />
+    <meta property="og:image" content="https://i0.wp.com/chefsbook.net/wp-content/uploads/2021/02/food-table-recipes.jpg?resize=932%2C512&ssl=1" />
     <title>Recipe Web App</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="style.css">
